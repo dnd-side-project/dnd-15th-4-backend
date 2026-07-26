8f6e8b555fef03aa544ffeabf87ad6c4d4e872e5
Hyochang098 <wlsgyckd7@naver.com>
2026-07-26 14:50:39 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/dnd/PuzzleMeet/PuzzleMeetApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.PuzzleMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/PuzzleMeet/PuzzleMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.PuzzleMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
